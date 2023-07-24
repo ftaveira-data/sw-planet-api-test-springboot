49410d845ccdeaef493f2be2bebb97e9130c7e93
ftaveira-dev <ftaveira.dev@gmail.com>
2023-07-24 20:14:06 -0300

    created test for create planet.

--- src/test/java/com/ftaveiradev/swplanetapi/domain/PlanetServiceTest.java
+++ src/test/java/com/ftaveiradev/swplanetapi/domain/PlanetServiceTest.java
@@ -1,19 +1,31 @@
 package com.ftaveiradev.swplanetapi.domain;
 import static com.ftaveiradev.swplanetapi.common.PlanetConstants.PLANET;
 import static org.assertj.core.api.Assertions.assertThat;
-
+import static org.mockito.Mockito.when;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 
-@SpringBootTest(classes = PlanetService.class)
+@ExtendWith(MockitoExtension.class)
+//@SpringBootTest(classes = PlanetService.class)
 public class PlanetServiceTest {
     //operacao_estado_resultado
 
-    @Autowired
+    //@Autowired
+    @InjectMocks
     private PlanetService planetService;
+
+    //@MockBean
+    @Mock
+    private PlanetRepository planetRepository;
     @Test
     public void createPlanet_WithValidData_ReturnPlanet() {
+        when(planetRepository.save(PLANET)).thenReturn(PLANET);
         //system under test
         Planet sut = planetService.create(PLANET);
 
